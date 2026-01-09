e437df6c042d0056609825ceb277b06a8d3548e6
busayoooo <abassalex49@gmail.com>
2026-01-09 14:12:12 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db/db");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Auth Backend Running");
+});
+
+app.get("/test", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT 1");
+    res.json({ message: "Database connected", rows });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = app;
